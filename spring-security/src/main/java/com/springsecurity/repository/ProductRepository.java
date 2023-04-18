9996f1c73671f4b7189138d14f57caa87c4c3c77
kishorek2511 <34577120+kishorek2511@users.noreply.github.com>
2023-04-18 17:45:22 +0530

    Delete spring-security directory

--- spring-security/src/main/java/com/springsecurity/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springsecurity.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springsecurity.dto.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer>{
-
-}
